77843c26d32507ade12a45505acac4876c22e453
excellnn <liu.cengjingcanghai@gmail.com>
2016-04-28 10:55:12 +0800

    新增once,unbind函数,改写publish,subscribe函数

--- index.js
+++ index.js
@@ -4,20 +4,63 @@ var Event = (function () {
     var _events = {};
 
     return {
+        /**
+         * 发布事件
+         * @param name  事件名
+         * @param obj   调用对象
+         * @returns {*} 结果
+         */
+
         publish: function publish(name, obj) {
-            return _events[name] && _events[name].call(obj);
+            var _result = undefined;
+            if (_events[name] !== undefined) {
+                if (obj !== undefined) {
+                    _result = _events[name].callback.call(obj);
+                } else {
+                    _result = _events[name].callback();
+                }
+                //如果是一次性事件,则在调用一次之后删除该事件
+                if (_events[name].type == 'once') {
+                    this.unbind(name);
+                }
+            }
+            return _result;
         },
-        subscribe: function subscribe(name, event) {
-            _events[name] = event;
+
+        /**
+         * 订阅事件
+         * @param name  事件名
+         * @param callback 回调函数
+         */
+        subscribe: function subscribe(name, callback) {
+            _events[name] = {
+                callback: callback,
+                type: 'all'
+            };
+            return this;
+        },
+
+        /**
+         * 订阅一次性事件
+         * @param name  事件名
+         * @param callback 回调函数
+         */
+        once: function once(name, callback) {
+            _events[name] = {
+                callback: callback,
+                type: 'once'
+            };
+            return this;
+        },
+
+        /**
+         * 取消事件
+         * @param name 事件名
+         */
+        unbind: function unbind(name) {
+            _events[name] = undefined;
         }
     };
 })();
-
-var Myobject = {};
-Event.subscribe('test', function () {
-    return console.log('test');
-});
-Event.publish('test');
-Event.publish('test');
 
 //# sourceMappingURL=index.js.map
